3b48d229ae4e76447b24ec87e806a0e265c5a2a6
MEGA <MEGA@gmail.com>
2025-02-28 15:12:08 +0200

    .

---
+++ ad6/Company.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ad6
+{
+    internal class Company
+    {
+        private List<Employee> employees;
+        public Company(List<Employee> employees)
+        {
+            this.employees = employees;
+            SelectionSort(this.employees);
+        }
+        private void SelectionSort(List<Employee> employees)
+        {
+            int n = employees.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (employees[j].Salary < employees[minIndex].Salary)
+                    {
+                        minIndex = j;
+                    }
+                }
+                if (minIndex != i)
+                {
+                    Employee temp = employees[i];
+                    employees[i] = employees[minIndex];
+                    employees[minIndex] = temp;
+                }
+            }
+        }
+
+        public string FindEmployeeBySalary(int salary)
+        {
+            int left = 0, right = employees.Count - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (employees[mid].Salary == salary)
+                    return employees[mid].Name;
+                else if (employees[mid].Salary < salary)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return "Не е намерен";
+        }
+    }
+}
---
+++ ad6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ad6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee("Ivan", 2300),
+                new Employee("Maria", 1900),
+                new Employee("Sofia", 3000),
+                new Employee("Aleksandar", 2600),
+                new Employee("Jordan", 1700)
+            };
+            Company company = new Company(employees);
+            Console.WriteLine("Въведете заплата за търсене: ");
+            int salary = int.Parse(Console.ReadLine());
+            string result = company.FindEmployeeBySalary(salary);
+            Console.WriteLine("Име на човека с тази заплата: " + result);
+            Console.ReadKey();
+        }
+    }
+}
